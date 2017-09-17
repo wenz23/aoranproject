e2a346eb3a9564c105049df0ca32b372d9879a65
ranaoyang <ranaoyang@gmail.com>
2017-09-17 11:43:33 -0700

    done

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='homepage.html'), name='homepage'),
+    url(r'^gym-list/$', TemplateView.as_view(template_name="gym/gym-list.html"), name='gym-list'),
 ]
