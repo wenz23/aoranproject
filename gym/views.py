e52f46971ff6ab41100b0d25111fa9f1940d97ee
ranaoyang <ranaoyang@gmail.com>
2017-09-17 13:10:35 -0700

    done

--- gym/views.py
+++ gym/views.py
@@ -5,7 +5,7 @@ from gym.models import Gyms
 
 def gym_list_view(request):
     if request.method == 'GET':
-        gyms = [[am.gym_name, am.street_address, am.zip_code, am.phone, am.revisited_at.isoformat(), am.gym_url] for am in Gyms.objects.all()]
+        gyms = [[am.gym_name, am.street_address, am.zip_code, am.phone, am.revisited_at.date().isoformat(), am.gym_url] for am in Gyms.objects.all()]
 
         return JsonResponse(gyms, safe=False)
 
