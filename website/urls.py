7238a86d5edb87b09d4f343dca8afb6d28d192fe
ranaoyang <ranaoyang@gmail.com>
2017-09-17 12:18:38 -0700

    done

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from gym import views as gview
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='homepage.html'), name='homepage'),
+    # url(r'^$', TemplateView.as_view(template_name='homepage.html'), name='homepage'),
     url(r'^gym-list/$', TemplateView.as_view(template_name="gym_list.html"), name='gym-list'),
     url(r'^get-gym-list/$', gview.gym_list_view)
 ]
