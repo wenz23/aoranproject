506bf9bbe47282e54a54ffa67e14d2578b764b50
ranaoyang <ranaoyang@gmail.com>
2017-09-26 20:26:16 -0700

    done

--- aws/bash/run.sh
+++ aws/bash/run.sh
@@ -4,5 +4,5 @@ echo Reset gunicorn	# This is a comment, too!
 PID=`ps -ef | grep python | grep -v "grep" | awk '{print $2}'`
 echo $PID
 kill -9 $PID
-. /var/aoranproject/aws/gunicorn_aws.sh &>/dev/null &
+. /var/aoranproject/aws/bash/gunicorn_aws.sh &>/dev/null &
 service nginx restart
