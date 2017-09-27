812d1ae564efc9fe3c55627799d55574f456beb3
ranaoyang <ranaoyang@gmail.com>
2017-09-26 19:58:29 -0700

    fix

--- aws/crontabs/ins_crawl_single_thread_02.sh
+++ aws/crontabs/ins_crawl_single_thread_02.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /Users/aoran/aoranproject/manage.py ins_crawl_single_thread --api P --settings=settings.production  >> /Users/aoran/aoranproject/logs/debug.log
+python /Users/aoran/aoranproject/manage.py ins_crawl_single_thread --api P --settings=settings.prod--uction  >> /Users/aoran/aoranproject/logs/debug.log
