907fe9ea6740cd8f25e8433a1f5a75267ef80818
ranaoyang <ranaoyang@gmail.com>
2017-10-02 10:40:39 -0700

    done

--- aoranproject/wsgi.py
+++ aoranproject/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
 
 application = Cling(get_wsgi_application())
