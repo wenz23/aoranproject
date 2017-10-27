c8b26628c23efc109ba1bdbad9388e2de70ed5f2
ranaoyang <ranaoyang@gmail.com>
2017-10-26 22:03:07 -0700

    google map

---
+++ crawl/integration/google_map_workflow.py
@@ -0,0 +1,106 @@
+from datetime import datetime
+import time
+import googlemaps
+
+from crawl.models import GoogleMap
+
+
+class GoogleMapParser:
+
+    def __init__(self):
+        self.key = 'AIzaSyDHcCKq5fKSIWYejt8PF0cO6P81L13xlcs'
+        self.g_maps = googlemaps.Client(key=self.key)
+        self.geo_dict = {}
+
+    def get_city_geocode(self, cities=None):
+        geo_dict = {}
+        for city in cities:
+            time.sleep(0.5)
+            city_geocode = self.g_maps.geocode(city)
+            lat_ne = city_geocode[0]['geometry']['viewport']['northeast']['lat']
+            lat_sw = city_geocode[0]['geometry']['viewport']['southwest']['lat']
+            lat_ct = (lat_ne + lat_sw) / 2
+
+            lng_ne = city_geocode[0]['geometry']['viewport']['northeast']['lng']
+            lng_sw = city_geocode[0]['geometry']['viewport']['southwest']['lng']
+            lng_ct = (lng_ne + lng_sw) / 2
+
+            geo_dict[city] = {'ct': {'lat': lat_ct, 'lng': lng_ct},
+                              # 'ne': {'lat': lat_ne, 'lng': lng_ne},
+                              # 'sw': {'lat': lat_sw, 'lng': lng_sw},
+                              # 'nw': {'lat': lat_ne, 'lng': lng_sw},
+                              # 'se': {'lat': lat_sw, 'lng': lng_ne}
+                              }
+
+        self.geo_dict = geo_dict
+
+    def search_place(self, queries=None):
+        city_geo_dict = self.geo_dict
+        for query in queries:
+            for key in city_geo_dict:
+                geo_dict = city_geo_dict[key]
+                for viewport in geo_dict:
+                    lat_lng = str(geo_dict[viewport]['lat']) + ", " + str(geo_dict[viewport]['lng'])
+
+                    next_page_token = ""
+                    while next_page_token is not None:
+                        results = self.g_maps.places(query=query, location=lat_lng, radius=50000, page_token=next_page_token)
+                        results = results['results']
+
+                        for result in results:
+                            place_id = result['place_id']
+                            gm_obj, created = GoogleMap.objects.get_or_create(place_id=place_id)
+                            if gm_obj.query is None or gm_obj.query == {}:
+                                gm_obj.query = {datetime.now().isoformat(): {'query': query, 'city': key}}
+                            else:
+                                temp_dict = gm_obj.query
+                                temp_dict[datetime.now().isoformat()] = {'query': query, 'city': key}
+                            gm_obj.save()
+
+                        try:
+                            next_page_token = results['next_page_token']
+                        except:
+                            next_page_token = None
+
+    def get_place_details(self):
+        places = [am for am in GoogleMap.objects.filter(place_id__isnull=False)]
+        for place in places:
+            result = self.g_maps.place(place.place_id)['result']
+            try:
+                place.map_url = result['url']
+            except:
+                pass
+            try:
+                place.name = result['name']
+            except:
+                pass
+            try:
+                place.rating = result['rating']
+            except:
+                pass
+            try:
+                place.reviews = len(result['reviews'])
+            except:
+                pass
+            try:
+                place.address = result['formatted_address']
+            except:
+                pass
+            try:
+                place.phone = result['formatted_phone_number']
+            except:
+                pass
+            try:
+                place.website = result['website']
+            except:
+                pass
+            try:
+                place.description = '; '.join(result['types'])
+            except:
+                pass
+            place.json = result
+            try:
+                place.save()
+            except Exception as e:
+                print(str(e))
+                pass
--- crawl/management/commands/gm_crawl.py
+++ crawl/management/commands/gm_crawl.py
@@ -1,134 +1,66 @@
-import random
-import time
-
 from django.core.management.base import BaseCommand
-from selenium import webdriver
-from crawl.models import GoogleMap
-from selenium.webdriver.common.keys import Keys
-
-
-def search_page(city_name=None, category_name=None):
-    mobile_emulation = {"deviceName": "iPhone 6 Plus"}
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-
-    driver = webdriver.Chrome('/Users/aoran/aoranproject/aws/chromedriver',
-                              desired_capabilities=chrome_options.to_capabilities())
-    driver.set_window_size(450, 900)
-    driver.get('http://www.google.com/xhtml')
-    time.sleep(random.uniform(4, 6))
-    input_box = driver.find_element_by_xpath('//*[@id="lst-ib"]')
-    input_box.click()
-    time.sleep(random.uniform(4, 6))
-    input_box.clear()
-    search_item = str(category_name) + " in " + str(city_name)
-    for i in search_item:
-        input_box.send_keys(i)
-        time.sleep(random.uniform(0.2, 0.6))
-
-    input_box.send_keys(Keys.RETURN)
-
-    time.sleep(random.uniform(4, 6))
-    return driver
-
-
-def nav_to_result_page(driver=None, max_page=10, high_rating=True):
-    time.sleep(random.randint(4, 6))
-    scroll = 0
-    times = 0
-    driver.execute_script("window.scrollTo(0, " + str(random.randint(290, 300)) + ");")
-    time.sleep(random.randint(4, 6))
-    more_places = driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div[2]/div[1]/div[3]/div/g-immersive-footer/g-fab/span')
-    more_places.click()
-    time.sleep(random.randint(4, 6))
-
-    while times < 50:
-        results = [am for am in driver.find_elements_by_xpath('//div[contains(@jsaction, "hero_carousel_call_to_action_card_shown")]') if am.is_displayed()]
-
-        for result in results:
-            try:
-                result.click()
-                time.sleep(random.randint(3, 5))
-
-                # Parse Here
-                driver = gm_parser(driver=driver)
-
-                driver.find_elements_by_xpath('//span[contains(@class,"kp-header-icon")]')[0].click()
-                time.sleep(random.randint(3, 5))
-            except Exception as e:
-                print(str(e))
-                pass
-
-        time.sleep(random.uniform(2, 4))
-        times += 1
-        scroll += 550
-        driver.execute_script("window.scrollTo(0, " + str(scroll) + ");")
-
-
-def gm_parser(driver=None):
-    try:
-        cid = driver.find_element_by_xpath('//div[@data-cid!=""][@data-fid!=""]').get_attribute('data-cid')
-    except Exception as e:
-        print(e)
-        cid=None
-        pass
-    try:
-        name = driver.find_element_by_xpath('//div[@style="opacity: 1; z-index: 2;"]').text
-    except Exception as e:
-        print(e)
-        name = None
-    try:
-        rating = driver.find_element_by_xpath('//span[contains(@class, "rtng")][@aria-hidden="true"]').text
-    except Exception as e:
-        print(e)
-        rating = None
-    try:
-        reviews = driver.find_element_by_xpath('//span[contains(@aria-label, " reviews")]').text
-    except Exception as e:
-        print(e)
-        reviews = None
-    try:
-        website = driver.find_element_by_xpath('//a[contains(@class, "lua-button")][contains(@href, "http")][contains(@ping, "url")]').get_attribute('href')
-    except Exception as e:
-        print(e)
-        website = None
-        pass
-    try:
-        descr = driver.find_element_by_xpath('//div[@role="button"][contains(@jsl, "$x 1;")]').text
-    except Exception as e:
-        print(e)
-        descr = None
-        pass
-    try:
-        address = driver.find_element_by_xpath('//div[@aria-label="Address:"]/following-sibling::div').text
-    except Exception as e:
-        print(e)
-        address = None
-        pass
-    try:
-        phone = driver.find_element_by_xpath('//span[@aria-label="Phone:"]/following-sibling::span').text
-    except Exception as e:
-        print(e)
-        phone = None
-        pass
-    try:
-        gm_obj, created = GoogleMap.objects.get_or_create(cid=cid)
-        gm_obj.name = name
-        gm_obj.phone = phone
-        gm_obj.rating = rating
-        gm_obj.reviews = reviews
-        gm_obj.website = website
-        gm_obj.description = descr
-        gm_obj.save()
-    except Exception as e:
-        print(e)
-        pass
-
-
-    return driver
+from crawl.integration.google_map_workflow import GoogleMapParser
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        driver = search_page(city_name='San Francisco', category_name='Boutique Shop')
-        nav_to_result_page(driver=driver)
+        top_us_cities = ['San Francisco, CA',
+                         'New York, NY',
+                         'Los Angeles, CA',
+                         'Chicago, IL',
+                         'Houston, TX',
+                         'Phoenix, AZ',
+                         'Philadelphia, PA',
+                         'San Antonio, TX',
+                         'San Diego, CA',
+                         'Dallas, TX',
+                         'San Jose, CA',
+                         'Austin, TX',
+                         'Seattle, WA',
+                         'Portland, OR',
+                         'Long Beach, CA',
+                         'Irvine, CA',
+                         'Miami, FL',
+                         'Charlotte, NC',
+                         'Denver, CO',
+                         'Washington, DC',
+                         'Boston, MA',
+                         'Memphis, TN',
+                         'Albuquerque, NM',
+                         'Atlanta, GA',
+                         'Newport Beach, CA',
+                         'New Orleans, LA',
+                         'Honolulu, HI',
+                         'St. Louis, MO',
+                         'Reno, NV',
+                         'Boise, ID',
+                         'Salt Lake City, UT',
+                         'Las Vegas, NV',
+                         'Milwaukee, WI',
+                         'Tucson, AZ',
+                         'Fresno, CA',
+                         'Sacramento, CA',
+                         'Colorado Springs, CO',
+                         'Virginia Beach, VA',
+                         'Oakland, CA',
+                         'Minneapolis, MN',
+                         'Kansas City, MO',
+                         'Arlington, VA',
+                         'Wichita, KS',
+                         'Cleveland, OH',
+                         'Tampa, FL',
+                         'Bakersfield, CA',
+                         'Pittsburgh, PA',
+                         'Orlando, FL',
+                         'Durham, NC']
+        queries = ['Clothing Store',
+                   'Boutique shop',
+                   'boutique fashion',
+                   'shoe store'
+                   'Designer',
+                   'Fashion Store']
+
+        gmap = GoogleMapParser()
+        gmap.get_city_geocode(cities=top_us_cities)
+        gmap.search_place(queries=queries)
+        gmap.get_place_details()
